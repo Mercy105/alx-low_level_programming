3aae9a47534210fbe41072f6866c94bdc075d73e
wawerumercy105 <wawerumercy105@gmail.com>
2023-06-16 02:16:05 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Programming is like building a multilingual puzzle
+ *Return: 0 (Sucess)
+ */
+int main(void)
+{
+	printf("/"with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
