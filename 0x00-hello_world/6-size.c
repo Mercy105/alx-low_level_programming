62032a1a56a543dcca5b8e19531b9f0c2a5a70c5
wawerumercy105 <wawerumercy105@gmail.com>
2023-06-16 23:46:25 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,5 @@ printf("Size of a int: %zu byte(s)\n", sizeof(b));
 printf("Size of a long int: %zu byte(s)\n", sizeof(c));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
 printf("Size of a float: %zu byte(s)", sizeof(f));
-        retun(0);
-
+retun(0);
 }
