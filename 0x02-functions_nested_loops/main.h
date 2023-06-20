7452c63a3ecb2e8fcb6892e1ecc93af6c9a698aa
wawerumercy105 <wawerumercy105@gmail.com>
2023-06-20 07:20:37 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,1 @@
 int _putchart(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void)
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauor(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void ptint_times_table(int n);
