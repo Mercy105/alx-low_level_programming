c89c235d4c18df00181078cb1ba2618ceae52ce2
wawerumercy105 <wawerumercy105@gmail.com>
2023-06-19 19:51:43 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <studio.h>
+
+/**
+ * main - print the alphabetic
+ * Return: Always (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c)
+	putchar('\n');
+	return (0);
+
+}
