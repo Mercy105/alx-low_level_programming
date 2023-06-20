f5bee2829bea5be4339d46fe2d122f888e31b693
wawerumercy105 <wawerumercy105@gmail.com>
2023-06-19 20:17:49 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	int n; lastd
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -17,15 +17,15 @@ int main(void)
 
 	if (lastd > 5)
 	{
-	printf("last digit of %d is and is greater than 5\n", n, lastd);
+	printf("last digit of %d is and is greater than 5\n", n);
 	}
 	else if (lastd == 0)
 	{
-	printf("last digit of %d is %d and is 0\n", n, lastd);
+	printf("last digit of %d is %d and is 0\n", n);
 	}
 	else if (lastd < 6 && lastd != 0)
 	{
-	printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+	printf("last digit of %d is %d and is less than 6 and not 0\n", n);
 	}
 	return (0);
 
