3d39baa0ccdb51af6d65eef7cb5766db6e72ade7
wawerumercy105 <wawerumercy105@gmail.com>
2023-06-20 09:55:41 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,22 @@ void print_alphabet_x10(void)
 {
 	char c;
 
-	int i = 0;
-	while (i <= 0)
+	int i;
+
+	i = 0;
+	while (i < 0)
 	{
-	for (c = 'a'; c <= 'z'; c++);
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+
+			c++;
+		}
+		_putchar('\n');
+
+		i++;
+
 	}
-	_putchar(c);
-	c++ ;
-	_putchar('\n');
-	i++ ;
 
 }
