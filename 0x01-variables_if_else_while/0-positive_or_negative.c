5b2c95cc9851dd3c960f71e985879daeb4e2fc2b
wawerumercy105 <wawerumercy105@gmail.com>
2023-06-19 19:09:40 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,6 +10,7 @@ int main(void)
 
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
@@ -19,10 +20,10 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-	printf("%d is zero\n", n):
+	printf("%d is zero\n", n);
 	}
 	else
-	{printf("%D is negative\n", n);
+	{printf("%d is negative\n", n);
 	}
 	return (0);
 }
