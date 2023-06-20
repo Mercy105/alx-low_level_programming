f97bc7127cad02e05fe3a4e26b64b4ec0fa28a8b
wawerumercy105 <wawerumercy105@gmail.com>
2023-06-20 06:49:58 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Fuctions and nested loops
+ *Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int _putchar(char c);
+
+	putchar(")
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+
+}
