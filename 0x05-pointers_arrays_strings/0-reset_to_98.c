32e313cace8031670229403a7f58fa3cf4088fb9
wawerumercy105 <wawerumercy105@gmail.com>
2023-06-27 03:57:35 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -> a function to update the preference to a pointer
+ * @n: int data type
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
