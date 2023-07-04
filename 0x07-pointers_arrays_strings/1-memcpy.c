8e8d713c91be9309b6cb7ab96386c2a12a938442
wawerumercy105 <wawerumercy105@gmail.com>
2023-07-04 03:11:57 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,29 +1,20 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * simple_print_buffer - print buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
+*_memcpy - copies memory area
+*@dest: destination to copy
+*@src: source pointer
+*@n: number of bytes to coppy
+*Return: dest (destination)
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+		unsigned int i;
 
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-		{ptintf(" ");
-		}
-		if (!(i % 10) && i)
+		for (i = 0; i < n; i++)
 		{
-			printf("\n");
+			dest[i] = src[i];
 		}
-		ptintf("0x%02x", buffer[1]);
-		i++;
-	}
+		return (dest);
 }
-
